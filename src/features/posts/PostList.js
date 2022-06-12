978046b447d8de449c71e989ccab699c258516f1
zhengshugan <zhengshugan@foxmail.com>
2022-06-12 20:25:42 +0800

    first commit

---
+++ src/features/posts/PostList.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+import { Spinner } from './../../components/Spinner';
+import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
+
+import {
+  fetchPosts,
+  selectPostIds,
+  selectPostById
+} from './postsSlice'
+
+let PostExcerpt = ({ postId }) => {
+  const post = useSelector((state) => selectPostById(state, postId))
+
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
+}
+
+export const PostsList = () => {
+  const dispatch = useDispatch()
+  const orderedPostIds = useSelector(selectPostIds)
+
+  const postStatus = useSelector(state => state.posts.status)
+  const error = useSelector(state => state.posts.error)
+
+  useEffect(() => {
+    if (postStatus === 'idle')
+      dispatch(fetchPosts())
+  }, [postStatus, dispatch])
+
+  let content
+
+  if (postStatus === 'loading') {
+    content = <Spinner text='Loading' />
+  } else if (postStatus === 'succeeded') {
+    // Sort posts in reverse chronological order by datetime string
+    content = orderedPostIds.map((postId) => (
+      <PostExcerpt key={postId} postId={postId} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
+  // Sort posts in reverse chronological order by datetime string
+  // const orderedPosts = posts
+  //   .slice()
+  //   .sort((a, b) => b.date.localeCompare(a.date))
+
+
+  return (
+    <section className="posts-list">
+      <h2>Posts</h2>
+      {content}
+    </section>
+  )
+}
+
+PostExcerpt = React.memo(PostExcerpt)
---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,37 @@
+import {
+  createAsyncThunk,
+  createSlice,
+  createEntityAdapter
+} from '@reduxjs/toolkit';
+import { client } from './../../api/client';
+
+const usersAdapter = createEntityAdapter()
+
+const initialState = usersAdapter.getInitialState()
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get('/fakeApi/users')
+  return response.data
+})
+
+const userSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: { },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchUsers.fulfilled, usersAdapter.setAll)
+  }
+})
+
+export default userSlice.reducer
+
+// export const selectAllUsers = state => state.users
+//
+// export const selectUserById = (state, userId) =>
+//   state.users.find(user => user.id === userId)
+
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById
+} = usersAdapter.getSelectors(state => state.users)
